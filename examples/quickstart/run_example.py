513098697425f7752def7331a73adf5ecb026d80
siforrer <80523026+siforrer@users.noreply.github.com>
2021-04-05 14:30:29 +0200

    Add examples

---
+++ examples/quickstart/run_example.py
@@ -0,0 +1,25 @@
+
+import sys
+from systemrdl import RDLCompiler
+from coreali import PythonExporter
+from coreali.registerio import RegIoNoHW
+
+
+# Compile register model with systemrdl-compiler
+rdlc = RDLCompiler()
+rdlc.compile_file("../systemrdl/i2c_master_core.rdl")
+root = rdlc.elaborate()
+
+# Generate hierarchical register model 
+pythonExporter = PythonExporter()
+pythonExporter.source_files = ["../systemrdl/i2c_master_core.rdl"]
+pythonExporter.export(root, "generated_regmodel.py")
+sys.path.insert(0, "generated/python/")
+
+
+import generated_regmodel
+i2c = generated_regmodel.i2c_master_core(RegIoNoHW())
+
+print(i2c)
+
+i2c.PRERlo.read()
