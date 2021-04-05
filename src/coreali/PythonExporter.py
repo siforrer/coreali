8b35a1d66ad931722c40af9f1316dd8269c1e903
Silvan Forrer <sforrer@gmx.net>
2021-04-05 16:29:03 +0200

    Write some documentation

--- src/coreali/PythonExporter.py
+++ src/coreali/PythonExporter.py
@@ -1,20 +1,21 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 11 09:50:05 2021
-
-@author: FSIL
-"""
 import os
 
 import jinja2 as jj
 from systemrdl.node import RootNode, Node, RegNode, AddrmapNode, RegfileNode
 from systemrdl.node import FieldNode, MemNode, AddressableNode
 
+
 class PythonExporter:
+    """ Exporter for a hierarchical register model in python
+    """
+
     def __init__(self):
+        """Create PythonExport class
+        """
 
         loader = jj.ChoiceLoader([
-            jj.FileSystemLoader(os.path.join(os.path.dirname(__file__), "templates")),
+            jj.FileSystemLoader(os.path.join(
+                os.path.dirname(__file__), "templates")),
             jj.PrefixLoader({
                 'base': jj.FileSystemLoader(os.path.join(os.path.dirname(__file__), "templates"))
             }, delimiter=":")
@@ -30,6 +31,12 @@ class PythonExporter:
         self.source_files = []
 
     def export(self, node: Node, path: str):
+        """Export register model to python file
+
+        Args:
+            node (Node): root node of hierarchical model
+            path (str): path of the newly generated register model file
+        """
         if isinstance(node, RootNode):
             node = node.top
         self.top = node
@@ -48,11 +55,10 @@ class PythonExporter:
             'source_files': self.source_files
         }
 
-
         template = self.jj_env.get_template("regmodel.py")
         stream = template.stream(context)
         stream.dump(path)
-        
+
     def _get_class_name(self, node: Node) -> str:
         class_name = node.get_rel_path(
             self.top.parent,
@@ -68,10 +74,10 @@ class PythonExporter:
             obj = self.namespace_db[type_name]
 
             if (obj is None) or (obj is not node.inst.original_def):
-                raise RuntimeError("Namespace collision! Type-name generation is not robust enough to create unique names!")
+                raise RuntimeError(
+                    "Namespace collision! Type-name generation is not robust enough to create unique names!")
 
             return False
 
         self.namespace_db[type_name] = node.inst.original_def
         return True
-
--- src/coreali/registerio/RegIo.py
+++ src/coreali/registerio/RegIo.py
@@ -1,40 +1,54 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Apr  4 22:11:56 2021
-
-@author: sforr
-"""
-
 import numpy as np
 
+
 class RegIo:
-    
+    """Base class for accessing registers
+
+    This base class handles the low level access to memory locations.
+    It provides some functions for word accesses. To implement a real 
+    register access a new class must be derived and this new class must 
+    implement the methods write_raw and read_raw. For an example of this 
+    please see the RegIoNoHW class.
+    """
+
     def read_word(self, address, word_size):
+        """Read a single word
+
+        Args:
+            address: address of register in byte
+            word_size: size of word in byte
+
+        Returns:
+            np.uint64: value of word at address
+        """
         ret = np.uint64(0)
-        raw_bytes = self.read_raw(address,word_size)
-        for i in range(word_size-1,-1,-1):
+        raw_bytes = self.read_raw(address, word_size)
+        for i in range(word_size-1, -1, -1):
             ret *= np.uint64(2**8)
             ret += raw_bytes[i]
         return np.uint64(ret)
-    
-    def write_word(self, address, word_size, word):
-        for i in range(word_size):
-            self.write_raw(address+i,[np.mod(word,256)])
-            word /= 256
-        
-    def read_words(self, address, word_size, num_words):
-        """ Read words starting from address
 
-        Parameters
-        ----------
-        address
-            address in bytes
-        num_elements
-            number of elements
-        word_size
-            number of bytes / word
+    def write_word(self, address, word_size, word):
+        """Write a single word
+
+        Args:
+            address: address of register in byte
+            word_size: size of word in byte
+            word: word that is written to locoation with address
         """
-        ret = np.empty([num_words],np.uint64)
+        for i in range(word_size):
+            self.write_raw(address+i, [np.mod(word, 256)])
+            word /= 256
+
+    def read_words(self, address, word_size, num_words):
+        """Read words starting from address
+
+        Args:
+            address: address of register in byte
+            word_size: size of one word in byte
+            num_words: number of words that are read
+        """
+        ret = np.empty([num_words], np.uint64)
         for i in range(num_words):
             ret[i] = self.read_word(address+i*word_size, word_size)
         return ret
@@ -42,15 +56,10 @@ class RegIo:
     def write_words(self, address, word_size, data):
         """Write words starting from address
 
-        Parameters
-        ----------
-        address
-            address in byte
-        word_size
-            Size of each word in bytes
-        data
-            array of words to be written starting from address
-
+        Args:
+            address: address of register in byte
+            word_size: size of one word in byte
+            data: array of words to be written starting from address
         """
         for idx, value in enumerate(data):
             self.write_word(address+idx*word_size, word_size, value)
